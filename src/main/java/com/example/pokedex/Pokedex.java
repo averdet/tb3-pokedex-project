8966d215309c954466e8554488dc953eed413edd
Alexandre <alexandre.verdet@etu.emse.fr>
2020-11-20 14:48:31 +0100

    retreive pokemon information from his id from pokeapi

--- src/main/java/com/example/pokedex/Pokedex.java
+++ src/main/java/com/example/pokedex/Pokedex.java
@@ -4,12 +4,26 @@ package com.example.pokedex;
 public class Pokedex {
 
     public static void main(String[] args) {
+        int id = 1;
+
         System.out.println("It's working !");
         if (args.length > 0) {
             System.out.println("Vous avez fourni l'argument " + args[0]);
+            try {
+                id = Integer.parseInt(args[0]);
+            } catch (NumberFormatException nfe) {
+                System.out.println("The first argument must be an integer.");
+                System.exit(1);
+            }
         }
-        SQLLiteExample.run();
-        HTTPRequestExample.run();
+
+        PokeApi pokeApi = new PokeApi();
+        Pokemon pokemon = pokeApi.retrievePokemonInfo(id);
+        String pokeInfo = pokemon.toString();
+        System.out.println(pokeInfo);
+
+        //SQLLiteExample.run();
+        //HTTPRequestExample.run();
     }
 
     public String getName() {
